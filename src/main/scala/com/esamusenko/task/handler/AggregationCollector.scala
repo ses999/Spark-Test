d7454dfd8f160ad5bb8e02386983385d9b4eb4f3
Evgenii Samusenko <essess199@gmail.com>
2020-07-22 17:22:56 +0300

    TASK-0001 optimize collector

--- src/main/scala/com/esamusenko/task/handler/AggregationCollector.scala
+++ src/main/scala/com/esamusenko/task/handler/AggregationCollector.scala
@@ -13,7 +13,12 @@ object AggregationCollector {
   def collect(df: DataFrame, param: TransformationParams): ResultElem = {
     implicit val enc: Encoder[(String, Long)] = Encoders.product[(String, Long)]
 
-    val map = df.filter(col(param.newColName).isNotNull).groupBy(col(param.newColName).cast(StringType)).agg(count(param.newColName)).map(r => r.getString(0) -> r.getLong(1)).collect().toMap
+    val map = df.select(col(param.newColName))
+      .filter(col(param.newColName).isNotNull)
+      .groupBy(col(param.newColName).cast(StringType))
+      .agg(count(param.newColName)).map(r => r.getString(0) -> r.getLong(1))
+      .collect()
+      .toMap
 
     ResultElem(param.newColName, map)
   }
